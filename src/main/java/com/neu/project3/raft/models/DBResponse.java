ed711211443c187563a0dc67bddc07cbf8a2b99a
Dikshant Rathi <drathi@ccs.neu.edu>
2020-11-29 22:03:56 -0500

    add db service

--- src/main/java/com/neu/project3/raft/models/DBResponse.java
+++ src/main/java/com/neu/project3/raft/models/DBResponse.java
@@ -1,4 +1,16 @@
 package com.neu.project3.raft.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class DBResponse {
+    private String key;
+    private String op;
+    private String value;
 }
--- src/main/java/com/neu/project3/raft/service/DatabaseService.java
+++ src/main/java/com/neu/project3/raft/service/DatabaseService.java
@@ -1,7 +1,39 @@
 package com.neu.project3.raft.service;
 
+import com.neu.project3.raft.models.DBRequest;
+import com.neu.project3.raft.models.DBResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class DatabaseService {
+
+    private volatile HashMap<String, String> db;
+
+    @Autowired
+    public DatabaseService(){
+        this.db = new HashMap<>();
+    }
+
+    public synchronized DBResponse getKey(DBRequest request){
+        if (db.containsKey(request.getKey())){
+            return new DBResponse(request.getKey(), request.getOp(), db.get(request.getKey()));
+        }
+        return new DBResponse(request.getKey(), request.getOp(), "NOT FOUND");
+    }
+
+    public synchronized DBResponse upsertKey(DBRequest request){
+        db.put(request.getKey(), request.getValue());
+        return new DBResponse(request.getKey(), request.getOp(), db.get(request.getKey()));
+    }
+
+    public synchronized DBResponse deleteKey(DBRequest request){
+        if (db.containsKey(request.getKey())){
+            db.remove(request.getKey());
+            return new DBResponse(request.getKey(), request.getOp(), "DELETED");
+        }
+        return new DBResponse(request.getKey(), request.getOp(), "NOT FOUND");
+    }
 }
